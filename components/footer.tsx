5ca05435da32f52d59909599ebe8859f72d5c3b3
Nikola Zlatkov <nikola.zlatkov@dualsoft.net>
2021-10-01 14:15:53 +0200

    Initial

---
+++ components/footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Image from 'next/image'
+
+import styles from '../styles/components/footer.module.scss'
+
+function Footer() {
+    return (
+        <footer className={styles.footer}>
+        footer
+      </footer>
+    )
+}
+
+export default Footer
---
+++ models/pages.ts
@@ -0,0 +1,5 @@
+export interface PageNav {
+    name: string;
+    url: 'bet-admin' | 'ibet2-desk' | 'ibet-web-client' | 'ibet2-mdc';
+    docs: string[];
+}
